2d1dd25adb4c6187a59724b91901bd8d5e535131
Ryan Stewart <ryanstewartalex@gmail.com>
2023-05-06 05:49:36 -0500

    Update

--- js/animations.js
+++ js/animations.js
@@ -18,7 +18,65 @@ for (let y = 0; y < numGridY; ++y)
 
         const index = y * numGridX + x;
         const rainbow = (360 * index / (numGridX * numGridY));
-        console.log(rainbow);
         squareContent.style.backgroundColor = "hsl(" + rainbow + ", 80%, 50%";
     }
 }
+
+function getPosition(el)
+{
+    let xPos = 0;
+    let yPos = 0;
+
+    while (el)
+    {
+        if (el.tagName === "body")
+        {
+            const xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+            const yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+            xPos += (el.offsetLeft - xScroll + el.clientLeft);
+            yPos += (el.offsetTop - yScroll + el.clientTop);
+        }
+        else
+        {
+            xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+            yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+        }
+
+        el = el.offsetParent;
+    }
+    return {
+        x: xPos,
+        y: yPos
+    };
+}
+
+const staggerValue = 1000;
+const opacityMin = .5;
+const scaleMin = .5;
+
+anime.timeline({
+    targets: ".grid-square-content",
+    easing: "easeInOutQuad",
+    loop: true
+}).add({
+    opacity: [1, opacityMin, 1],
+    scale: [1, scaleMin, 1],
+    borderRadius: [0, "50%", 0],
+    delay: anime.stagger([0, staggerValue], {grid: [16, 16]})
+}).add({
+    opacity: [1, opacityMin, 1],
+    scale: [1, scaleMin, 1],
+    borderRadius: [0, "50%", 0],
+    delay: anime.stagger([staggerValue, 0], {grid: [16, 16]})
+}).add({
+    opacity: [1, opacityMin, 1],
+    scale: [1, scaleMin, 1],
+    borderRadius: [0, "50%", 0],
+    delay: anime.stagger([0, staggerValue / 2], {grid: [16, 16], from: "center"})
+}).add({
+    opacity: [1, opacityMin, 1],
+    scale: [1, scaleMin, 1],
+    borderRadius: [0, "50%", 0],
+    delay: anime.stagger([staggerValue / 2, 0], {grid: [16, 16], from: "center"})
+});
